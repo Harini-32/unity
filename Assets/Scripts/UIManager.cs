0e243e379b72a445845b400606ada79d8588b78b
Harini Anumandla <harinianumandla@gmail.com>
2021-03-13 11:59:56 -0500

    Main Menu

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void DisableBoolInAnimator(Animator anim)
+    {
+        anim.SetBool("isDisplayed", false);
+    }
+
+    public void EnableBoolAnimator(Animator anim)
+    {
+        anim.SetBool("isDisplayed", true);
+    }
+
+       public void NavigateTo(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
